1bb5a4092402b81270bac3a5a11a113a324d5137
rajareddy <rajareddy.papi@gmail.com>
2020-02-07 10:09:58 +0530

    First Commit

---
+++ src/abc/Test.java
@@ -0,0 +1,16 @@
+package abc;
+
+public class Test {
+	public static void main(String[] args)
+	{
+		String hungry = "yes";
+		
+		if(hungry == "yes")
+			System.out.println("eating");
+		else {
+			System.out.println("not eating");
+		}
+		
+	}
+
+}
